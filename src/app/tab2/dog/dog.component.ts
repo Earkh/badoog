2259e03fbfba907e619517d2862b6125ce5254ae
Airam <airam.dev@gmail.com>
2021-03-16 12:44:27 +0000

    Añadidas animaciones

--- src/app/tab2/dog/dog.component.ts
+++ src/app/tab2/dog/dog.component.ts
@@ -4,9 +4,9 @@ import { SliceService } from 'src/app/services/slice.service';
 import { ModalPage } from '../modal/modal.page'
 
 @Component({
-  selector: 'app-dog',
-  templateUrl: './dog.component.html',
-  styleUrls: ['./dog.component.scss'],
+    selector: 'app-dog',
+    templateUrl: './dog.component.html',
+    styleUrls: ['./dog.component.scss'],
 })
 export class DogComponent implements OnInit {
 
@@ -18,34 +18,42 @@ export class DogComponent implements OnInit {
     @Input() desc: string;
     @Input() img: string;
 
-  constructor(
-              private modalController: ModalController,
-              private sliceService: SliceService
-              ) { }
+    constructor(
+        private modalController: ModalController,
+        private sliceService: SliceService
+    ) { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
-  async presentModal() {
-    const modal = await this.modalController.create({
-        component: ModalPage,
-        componentProps: {
-            name: this.name,
-            sex: this.sex,
-            size: this.size,
-            age: this.age,
-            desc: this.desc
-        }
-    });
-    return await modal.present();
-  }
+    async presentModal() {
+        const modal = await this.modalController.create({
+            component: ModalPage,
+            componentProps: {
+                name: this.name,
+                sex: this.sex,
+                size: this.size,
+                age: this.age,
+                desc: this.desc
+            }
+        });
+        return await modal.present();
+    }
 
-  like(event) {
-    console.log("User (UserId) likes " + this.name + "(" + this.id + ")");
-    this.sliceService.increaseS();
-  }
+    like(event) {
+        console.log("User (UserId) likes " + this.name + "(" + this.id + ")");
+        document.getElementById('card').classList.remove('animate__fadeInRight')
+        document.getElementById('card').classList.add('animate__fadeOutLeft')
+        setTimeout(() => {
+            this.sliceService.increaseS();
+        }, 600)
+    }
 
-  dislike(event){
-    console.log("User (UserId) dislikes " + this.name + "(" + this.id + ")");
-    this.sliceService.increaseS();
-  }
+    dislike(event) {
+        console.log("User (UserId) dislikes " + this.name + "(" + this.id + ")");
+        document.getElementById('card').classList.remove('animate__fadeInRight')
+        document.getElementById('card').classList.add('animate__fadeOutLeft')
+        setTimeout(() => {
+            this.sliceService.increaseS();
+        }, 600)
+    }
 }
